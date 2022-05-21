7d354aceac7fc5f3fdc99eefc92981a5d0d955c4
rachmaadji <104288914+rachmaadji@users.noreply.github.com>
2022-05-21 07:02:54 +0700

    Create latihan5.py

---
+++ pertemuan-2/latihan5.py
@@ -0,0 +1,7 @@
+#Satuan Tinggi
+print("Konversi Satuan Tinggi")
+kaki = float(input("Masukan tinggi ( kaki) :"))
+inci = float(input("Masukan tinggi (inch) :"))
+print(kaki,"kaki = ",kaki*12, "inch \n ", 
+      kaki,"kaki = ", kaki*12*2.54,"cm \n ", 
+      inci,"inch =",inci*2.54,"cm" )
