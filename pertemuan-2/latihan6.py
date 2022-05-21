d0492297cd87bd9e22cbc547164577e6070bebd0
rachmaadji <104288914+rachmaadji@users.noreply.github.com>
2022-05-21 07:05:11 +0700

    Create latihan6.py

---
+++ pertemuan-2/latihan6.py
@@ -0,0 +1,6 @@
+#Satuan Jarak
+print("Konversi Satuan Jarak")
+kaki = float(input("Masukan tinggi ( kaki) :"))
+print(kaki,"kaki = ",kaki*12, "inch \n ", 
+      kaki,"kaki = ", kaki*0.3333,"yard \n ", 
+      kaki,"kaki =",inci*0.00018939,"mil" )
