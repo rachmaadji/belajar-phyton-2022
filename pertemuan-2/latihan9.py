d18a3b3b083ea82ac67cf8a964813cc6aeec3705
rachmaadji <104288914+rachmaadji@users.noreply.github.com>
2022-05-21 07:07:58 +0700

    Create latihan9.py

---
+++ pertemuan-2/latihan9.py
@@ -0,0 +1,6 @@
+#Unit Tekanan
+print("Konversi satuan tekanan")
+Tekanan = float(input("Masukan tekanan (kilopascal) :"))
+print(Tekanan, "kPa = ", Tekanan*0.145038, "psi \n",
+      Tekanan, "kPa = ", Tekanan*7.50062, "mmHg \n",
+      Tekanan, "kPa = ", Tekanan*0.00986923, "atm \n")
