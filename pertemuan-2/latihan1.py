20d403a28d36634de9c453c3ee710063f0c71877
rachmaadji <104288914+rachmaadji@users.noreply.github.com>
2022-05-21 07:19:45 +0700

    Create latihan1.py

---
+++ pertemuan-2/latihan1.py
@@ -0,0 +1,11 @@
+    nama = "Kim Taehyung"
+    ttl = "Daegu, 30 Desember 1995"
+    namalain = "V"
+    hobi = "bermain golf"
+print(
+      "======================================== \n",
+      "Biodata Singkat V BTS \n",
+      "Nama :", nama, "\n"
+      "Tempat,Tanggal lahir :",ttl, "\n",
+      "Nama Lain (BTS) :",namalain,"\n"
+      "Hobi :", hobi)
